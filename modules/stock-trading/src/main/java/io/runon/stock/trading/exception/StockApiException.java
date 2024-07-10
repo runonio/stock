60266bfc591f5dd399de06079e55feb37e719c8c
macle <ysys86a@gmail.com>
2024-07-10 11:39:17 +0900

    2024

--- modules/stock-trading/src/main/java/io/runon/stock/trading/exception/StockApiException.java
+++ modules/stock-trading/src/main/java/io/runon/stock/trading/exception/StockApiException.java
@@ -1,10 +1,13 @@
 package io.runon.stock.trading.exception;
+
+import io.runon.trading.exception.TradingApiException;
+
 /**
  * api를 활용하다 예상하지 않은 예외가 발생했을때
  * 예외가 명확히 정의되지 않은 케이스
  * @author macle
  */
-public class StockApiException extends RuntimeException{
+public class StockApiException extends TradingApiException {
 
     /**
      * 생성자
