710b17b6659f31b7eb4fbffea755d2879523e017
macle <ysys86a@gmail.com>
2024-10-14 12:58:10 +0900

    2024

---
+++ modules/stock-trading/src/test/java/dev/RunonDataSync.java
@@ -0,0 +1,27 @@
+package dev;
+
+import io.runon.stock.trading.country.kor.KorStocks;
+import io.runon.stock.trading.data.management.KorSpotDailyShortSellingOut;
+import io.runon.stock.trading.data.management.KorSpotDailyStockLoanOut;
+import io.runon.stock.trading.data.management.db.sync.StockDbSync;
+import io.runon.trading.data.api.ApiDataSync;
+
+public class RunonDataSync {
+    public static void main(String[] args) {
+
+        StockDbSync.getInstance().sync();
+        KorStocks.updateStockType();
+        ApiDataSync.candleSyncAll();
+
+        //대차잔고
+        KorSpotDailyStockLoanOut stockLoanOut = new KorSpotDailyStockLoanOut();
+        stockLoanOut.outKor();
+        stockLoanOut.outKorDelisted();
+//
+//            //공매도
+        KorSpotDailyShortSellingOut shortSellingOut = new KorSpotDailyShortSellingOut();
+        shortSellingOut.outKor();
+        shortSellingOut.outKorDelisted();
+    }
+
+}
